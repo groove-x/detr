c78cb66a7646fa41b65a322fdeca5e55f709456a
katsunori waragai <katsunori.waragai@groove-x.com>
2023-06-01 19:02:30 +0900

    just add converter fucntion

--- detect.py
+++ detect.py
@@ -105,6 +105,19 @@ def cv2pil(image):
     new_image = Image.fromarray(new_image)
     return new_image.copy()
 
+
+def pil2cv(image):
+    ''' PIL型 -> OpenCV型 '''
+    new_image = np.array(image, dtype=np.uint8)
+    if new_image.ndim == 2:  # モノクロ
+        pass
+    elif new_image.shape[2] == 3:  # カラー
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_RGB2BGR)
+    elif new_image.shape[2] == 4:  # 透過
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_RGBA2BGRA)
+    return new_image
+
+
 if __name__ == "__main__":
     import argparse
     from pathlib import Path
